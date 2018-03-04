3e7758cd4a9408b93cc135d55507b1dc826a9ae9
bmh10 <bmhomer13@gmail.com>
2018-03-04 09:59:04 +0000

    Tidy code

--- Main.hs
+++ Main.hs
@@ -28,16 +28,12 @@ background = black
 
 data LifeGame = Game
   { 
-    particles :: [Particle],
-    objects :: [Particle],
+    objects :: [Object],
     paused :: Bool,
-    particleCreationCol :: G2.Color,
-    gravity :: Int,
-    showStats :: Bool,
     gen :: StdGen
   } deriving Show 
 
-data Particle = Particle
+data Object = Object
   {
     pos :: (Int, Int),
     vel :: (Int, Int),
@@ -46,39 +42,18 @@ data Particle = Particle
     col :: G2.Color
   } deriving Show
 
-particle pos vel mass radius col = Particle { pos = pos, vel = vel, mass = mass, radius = radius, col = col }
-
-randomParticles 0 gen col grav = ([], gen)
-randomParticles n gen col grav = (p : ps, gen'')
-  where (p, gen')   = randomParticle gen col grav
-        (ps, gen'') = randomParticles (n-1) gen' col grav
-
-randomParticle gen col grav = (particle (x, y) (xvel, mass*grav) mass particleRadius col, gen''')
-  where (x, y, gen')   = randomPos gen
-        (xvel, gen'')  = randomVel gen'
-        (mass, gen''') = randomMass gen''
-
-randomPos gen = (x, y, gen'')
-  where (x, gen')  = randomR (-w, w)  gen
-        (y, gen'') = randomR (h-50, h+50) gen'
-        w = quot width 2
-        h = quot height 2
-
-randomVel gen  = randomR (-xVelRange, xVelRange) gen
-randomMass gen = randomR (1, 5) gen
-
 -- Rendering
 render :: LifeGame -> Picture 
-render g = pictures [renderParticles g, 
-                     if (showStats g) then renderDashboard g else blank]
+render g = pictures [renderObjects g, 
+                     renderDashboard g]
 
 renderDashboard :: LifeGame -> Picture
-renderDashboard g = G2.color white $ translate (-300) (-fromIntegral height/2 + 5) $ scale 0.1 0.1 $ text $ "Particles: " ++ show (length (particles g))
+renderDashboard g = G2.color white $ translate (-300) (-fromIntegral height/2 + 5) $ scale 0.1 0.1 $ text $ "Dashboard"
 
-renderParticles g = pictures $ map renderParticle (objects g) ++ map renderParticle (particles g)
+renderObjects g = pictures $ map renderObject (objects g)
 
-renderParticle :: Particle -> Picture
-renderParticle p
+renderObject :: Object -> Picture
+renderObject p
  = translate x' y' $ G2.color (col p) $ circleSolid (radius p)
   where
     (x, y) = pos p
@@ -88,75 +63,24 @@ renderParticle p
 -- Event handling
 handleKeys :: Event -> LifeGame -> LifeGame
 handleKeys (EventKey (Char 'p') Down _ _) g = togglePaused g
-handleKeys (EventKey (Char 'c') Down _ _) g = toggleParticleColor g
-handleKeys (EventKey (Char 'g') Down _ _) g = increaseGravity g
-handleKeys (EventKey (Char 'r') Down _ _) g = reset g
-handleKeys (EventKey (Char 's') Down _ _) g = toggleStats g
 handleKeys _ game = game
 
-toggleStats g = g { showStats = not (showStats g) }
 togglePaused g = g { paused   = not (paused g) }
-increaseGravity g = g { gravity = (gravity g)-1 }
-toggleParticleColor g = g { particleCreationCol = nextCol (particleCreationCol g)}
-  where nextCol c 
-          | c == blue  = red
-          | c == red   = green
-          | c == green = blue
 
 update :: Float -> LifeGame -> LifeGame
 update secs game
  | (paused game) = game
  | otherwise     = updateGame game
 
-updateGame g = addParticles $ g { particles = updateParticles (particles g) (objects g) }
-
-addParticles g = g { particles = (particles g) ++ ps, gen = gen' }
-  where (ps, gen') = randomParticles extraParticlesPerFrame (gen g) (particleCreationCol g) (gravity g)
-
-updateParticles [] _ = []
-updateParticles (p:ps) os = updateParticle p os ++ updateParticles ps os
-  where updateParticle p os
-          | willHitObject p os = [p { pos = add (pos p) (0, -1), col = red } ]
-          | willHitWall p = [p { vel = bounceX (vel p), col = orange} ]
-          | inRange p = [p { pos = add (pos p) (vel p) }]
-          | otherwise = []
-
-bounceX (x, y) = (-x, y)
-
-willHitObject :: Particle -> [Particle] -> Bool
-willHitObject _ [] = False
-willHitObject p (o:os) = willHitObject' p o || willHitObject p os
-  where 
-        willHitObject' :: Particle -> Particle -> Bool
-        willHitObject' p o = d < (radius o)
-        d = sqrt $ fromIntegral ((px - cx)*(px - cx) + (py - cy)*(py - cy))
-        (px, py) = pos p
-        (cx, cy) = pos o
-
-willHitWall p = x < -w || x > w
-  where (x, y) = add (pos p) (vel p)
-        w = quot width 2
-
-inRange p = -w <= x && x <= w && -h <= y && y <= h
-  where (x, y) = pos p
-        w = quot width 2
-        h = quot height 2
-
-add (a,b) (c,d) = (a+c,b+d)
+updateGame g = g
 
 initialObstacles = [obs (0, 0)]
 
-obs p = Particle { pos = p, vel = (0, 0), mass = 1, radius = 50, col = yellow }
-
-initParticles gen = randomParticles numInitialParticles gen blue initialGravity
-
-reset g = g { paused = False, particles = initialParticles, objects = initialObstacles, gen = gen', particleCreationCol = blue, gravity = initialGravity }
-  where (initialParticles, gen') = initParticles (gen g)
+obs p = Object { pos = p, vel = (0, 0), mass = 1, radius = 50, col = yellow }
 
 initGame = do 
   stdGen <- newStdGen
-  let (initialParticles, stdGen') = initParticles stdGen
-  let initialState = Game { paused = False, particles = initialParticles, objects = initialObstacles, gen = stdGen', particleCreationCol = blue, gravity = initialGravity, showStats = False }
+  let initialState = Game { paused = False, objects = initialObstacles, gen = stdGen }
   return initialState
 
 main = do
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ haskell-solar
 
 ![screenshot](img/screenshot.png)
 
+References:
+http://www.enchantedlearning.com/subjects/astronomy/planets/
